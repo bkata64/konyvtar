6adb257a1bbf83cb99ff1a19816a6bf732af0258
bkata64 <bkata64@gmail.com>
2022-03-24 09:40:46 +0100

    kategoria szamlalas jav.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    fantasyDb = 0;    
-    motivaciosDb = 0;
-    regenyDb = 0;
+    szamlalok = {}
+    elemek = $('#kategoriak').find('input');
+    for (elem of elemek){
+        szamlalok[elem['id']] = 0;        
+    }
+
     if (localStorage.getItem("panel")=="closed") {
         $('#kategoria').addClass('rejtett');
         $('.rejtett').css('width','0');
@@ -31,25 +34,36 @@ $(document).ready(function(){
     $('#megse').click(function(){
         $(this).hide();
     });
-    $('#fantasy').click(function(){
-        if($('#fantasy').prop('checked')) {
-            fantasyDb++;        
-            sessionStorage.setItem('fantasy',fantasyDb);
-            $('#fantasy_lbl').text("Fantasy (" + fantasyDb + ")");
-        }       
-    });
-    $('#motivacios').click(function(){
-        if($('#motivacios').prop('checked')) {
-            motivaciosDb++;        
-            sessionStorage.setItem('motivacios',motivaciosDb);
-            $('#motivacios_lbl').text("Motivációs (" + motivaciosDb + ")");
-        }       
-    });
-    $('#regeny').click(function(){
-        if($('#regeny').prop('checked')) {
-            regenyDb++;        
-            sessionStorage.setItem('regeny',regenyDb);
-            $('#regeny_lbl').text("Regény (" + regenyDb + ")");
-        }       
+    // $('#fantasy').click(function(){
+    //     if($('#fantasy').prop('checked')) {
+    //         fantasyDb++;        
+    //         sessionStorage.setItem('fantasy',fantasyDb);
+    //         $('#fantasy_lbl').text("Fantasy (" + fantasyDb + ")");
+    //     }       
+    // });
+    // $('#motivacios').click(function(){
+    //     if($('#motivacios').prop('checked')) {
+    //         motivaciosDb++;        
+    //         sessionStorage.setItem('motivacios',motivaciosDb);
+    //         $('#motivacios_lbl').text("Motivációs (" + motivaciosDb + ")");
+    //     }       
+    // });
+    // $('#regeny').click(function(){
+    //     if($('#regeny').prop('checked')) {
+    //         regenyDb++;        
+    //         sessionStorage.setItem('regeny',regenyDb);
+    //         $('#regeny_lbl').text("Regény (" + regenyDb + ")");
+    //     }       
+    // });
+    $('#kategoriak').on('click', 'input', function(event){
+        //azonosito = $(event.target).prop('id');
+        if(event.target['checked']){
+            const azonosito = event.target['id'];
+            szamlalok[azonosito]++;
+            const cimke = '#' + azonosito + '_lbl'
+            const kiirando = $(event.target).data('szoveg')
+            sessionStorage.setItem(azonosito,szamlalok[azonosito]);
+            $(cimke).text(`${kiirando} (${szamlalok[azonosito]})`);
+        }
     });
 });
